8225b5b056732128e4579c6e6061ded49d608ed8
Olga Mkrtchyan <olia.mst@gmail.com>
2021-08-19 23:05:39 +0300

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,8 @@
+
+document.getElementsByClassName(".header").height = window.height;
+
+$(".navbar a").click(function(){
+    $("body,html").animate({
+        scrollTop:$("#"+(this).data('value')).offset().top
+    }, 1000)
+})
